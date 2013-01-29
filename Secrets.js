b042842abe3b979625741e6f6a6a9a38e2962408
Nathan Wall <nwall@appnexus.com>
2013-01-29 00:52:40 -0500

    Trimmed down SymbolsForES5 to just provide Secrets.

---
+++ Secrets.js
@@ -0,0 +1,290 @@
+var createSecret = (function(Object, String) {
+
+	// We capture the built-in functions and methods as they are now and store them as references so that we can
+	// maintain some integrity. This is done to prevent scripts which run later from mischievously trying to get
+	// details about or alter the secrets stored on an object.
+	var lazyBind = Function.prototype.bind.bind(Function.prototype.call),
+
+		// ES5 functions
+		create = Object.create,
+		getPrototypeOf = Object.getPrototypeOf,
+		isExtensible = Object.isExtensible,
+		isFrozen = Object.isFrozen,
+		freeze = Object.freeze,
+		keys = Object.keys,
+		getOwnPropertyNames = Object.getOwnPropertyNames,
+		getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor,
+		defineProperty = Object.defineProperty,
+		hasOwn = lazyBind(Object.prototype.hasOwnProperty),
+		push = lazyBind(Array.prototype.push),
+		forEach = lazyBind(Array.prototype.forEach),
+		map = lazyBind(Array.prototype.map),
+		filter = lazyBind(Array.prototype.filter),
+		join = lazyBind(Array.prototype.join),
+		fromCharCode = String.fromCharCode,
+		apply = lazyBind(Function.prototype.apply),
+		bind = lazyBind(Function.prototype.bind),
+
+		// ES Harmony functions
+		getPropertyNames = Object.getPropertyNames,
+
+		// ES.next strawman functions
+		getPropertyDescriptors = Object.getPropertyDescriptors,
+		getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors,
+
+		// ES Harmony constructors
+		_Proxy = typeof Proxy == 'undefined' ? undefined : Proxy,
+
+		// Determines whether object[SECRET_KEY] should expose the secret map.
+		locked = true,
+
+		random = getRandomGenerator(),
+		MIN_PRECISION = -Math.pow(2, 53),
+		MAX_PRECISION = -MIN_PRECISION,
+		// idNum will ensure identifiers are unique.
+		idNum = [ MIN_PRECISION ],
+		preIdentifier = randStr(7) + '0',
+		SECRET_KEY = '!S:' + getIdentifier();
+
+	(function() {
+		// Override get(Own)PropertyNames and get(Own)PropertyDescriptors to hide SECRET_KEY.
+
+		var overrides = create(null);
+
+		overrides.getOwnPropertyNames = getOwnPropertyNames
+		if (getPropertyNames) overrides.getPropertyNames = getPropertyNames;
+
+		keys(overrides).forEach(function(u) {
+			var original = overrides[u];
+			defineProperty(Object, u, {
+				value: function(obj) {
+					return filter(apply(original, this, arguments), function(u) {
+						return u != SECRET_KEY;
+					});
+				},
+				enumerable: false,
+				writable: true,
+				configurable: true
+			});
+		});
+
+		overrides = create(null);
+
+		if (getPropertyDescriptors) overrides.getPropertyDescriptors = getPropertyDescriptors;
+		if (getOwnPropertyDescriptors) overrides.getOwnPropertyDescriptors = getOwnPropertyDescriptors;
+
+		keys(overrides).forEach(function(u) {
+			var original = overrides[u];
+			defineProperty(Object, u, {
+				value: function(obj) {
+					var desc = apply(original, this, arguments);
+					delete desc[SECRET_KEY];
+					return desc;
+				},
+				enumerable: false,
+				writable: true,
+				configurable: true
+			});
+		});
+
+	})();
+
+	// Override functions which prevent extensions on objects to go ahead and add a secret map first.
+	[ 'preventExtensions', 'seal', 'freeze' ].forEach(function(u) {
+		var original = Object[u];
+		defineProperty(Object, u, {
+			value: function(obj) {
+				// Define the secret map.
+				Secrets(obj);
+				return apply(original, this, arguments);
+			}
+		});
+	});
+
+	if (typeof _Proxy == 'function') {
+
+		Proxy = (function() {
+			/* TODO: This works for "direct_proxies", the current ES6 draft; however, some browsers have
+			 * support for an old draft (such as FF 17 and below) which uses Proxy.create(). Should this
+			 * version be overridden to protect against discovery of SECRET_KEY on these browsers also?
+			 */
+
+			var trapBypasses = create(null);
+			trapBypasses.defineProperty = defineProperty;
+			trapBypasses.hasOwn = hasOwn;
+			trapBypasses.get = function(target, name) { return target[name]; };
+
+			return function Proxy(target, traps) {
+
+				if (!(this instanceof Proxy)) {
+					// TODO: The new keyword wasn't used. What should be done?
+					return new Proxy(target, traps);
+				}
+
+				var _traps = create(traps);
+
+				forEach(keys(trapBypasses), function(trapName) {
+					var bypass = trapBypasses[trapName];
+					if (typeof traps[trapName] == 'function') {
+						// Override traps which could discover SECRET_KEY.
+						_traps[trapName] = function(target, name) {
+							if (name === SECRET_KEY) {
+								// Bypass any user defined trap when name === SECRET_KEY.
+								return apply(bypass, null, arguments);
+							}
+							return apply(traps[trapName], this, arguments);
+						};
+					}
+				});
+
+				return new _Proxy(target, _traps);
+			};
+
+		})();
+
+	} else if (_Proxy && _Proxy.create) {
+
+//		Proxy.create = (function() {
+//
+//			return function create(traps, proto) {
+//				// TODO
+//			};
+//
+//		})();
+
+	}
+	
+	return function createSecret() {
+
+		var id = nextUniqueId();
+
+		return function secret(obj) {
+			var secrets = Secrets(obj);
+			if (secrets)
+				return secrets[id] || secrets[id] = create(null);
+			else
+				// The object may have been frozen in another frame.
+				throw new Error('This object doesn\'t support secrets.');
+		};
+
+	};
+
+	function Secrets(O) {
+		// Returns undefined if object doesn't already have Secrets and the object is non-extensible. This should
+		// really only happen if an object is passed in from another frame, because in this frame preventExtensions
+		// is overridden to add a Secrets property first.
+
+		if(O === Object.prototype) return;
+		if (O !== Object(O)) throw new Error('Not an object: ' + O);
+		if (!hasOwn(O, SECRET_KEY)) {
+			if (!isExtensible(O)) return;
+			defineProperty(O, SECRET_KEY, own({
+
+				get: (function() {
+					var secretMap = create(null);
+					return function getSecret() {
+						var value;
+						// The lock protects against retrieval in the event that the SECRET_KEY is discovered.
+						if (locked) return;
+						locked = true;
+						return secretMap;
+					};
+				})(),
+
+				enumerable: false,
+				configurable: false
+
+			}));
+		}
+		locked = false;
+		
+		return O[SECRET_KEY];
+
+	}
+
+	function getIdentifier() {
+		return preIdentifier + ':' + join(getRandStrs(8, 11), '/') + ':' + nextUniqueId();
+	}
+
+	function nextUniqueId() {
+		idNum[0]++;
+		for(var i = 0; idNum[i] >= MAX_PRECISION && i < idNum.length; i++) {
+			idNum[i] = MIN_PRECISION;
+			if (i < idNum.length) idNum[i + 1]++;
+			else {
+				// Reset and add a digit.
+				idNum = map(idNum, function() { return MIN_PRECISION; });
+				push(idNum, MIN_PRECISION);
+				break;
+			}
+		}
+		return '{' + join(idNum, ',') + '}';
+	}
+
+	function encodeStr(num) {
+		return fromCharCode(num + 65);
+	}
+
+	function getRandStrs(count, length) {
+		var r = [ ];
+		for(var i = 0; i < count; i++) {
+			push(r, randStr(length));
+		}
+		return r;
+	}
+
+	function randStr(length) {
+		var s = '';
+		for (var i = 0; i < length; i++) {
+			s += encodeStr(random() * (125 - 65 + 1));
+		}
+		return s;
+	}
+
+	function getRandomGenerator() {
+		var getRandomValues
+			= typeof crypto != 'undefined' && crypto != null
+				? (function() {
+					var f = crypto.random || crypto.getRandomValues;
+					if (f) return f.bind(crypto);
+					return undefined;
+				})()
+				: undefined;
+		if (getRandomValues) {
+			// Firefox (15 & 16) seems to be throwing a weird "not implemented" error on getRandomValues.
+			// Not sure why?
+			try { getRandomValues(new Uint8Array(4)); }
+			catch(x) { getRandomValues = undefined }
+		}
+		if (typeof getRandomValues == 'function' && typeof Uint8Array == 'function') {
+			return (function() {
+				var values = new Uint8Array(4), index = 4;
+				return function random() {
+					if (index >= values.length) {
+						getRandomValues(values);
+						index = 0;
+					}
+					return values[index++] / 256;
+				};
+			})();
+		} else return Math.random;
+	}
+
+	function own(obj) {
+
+		var O = create(null);
+
+		forEach(getOwnPropertyNames(obj), function(key) {
+			defineProperty(O, key,
+				getOwnPropertyDescriptor(obj, key));
+		});
+
+		return O;
+
+	}
+
+// We pass in Object and String to ensure that they cannot be changed later to something else.
+})(Object, String);
+
+if (typeof exports != 'undefined' && exports != null)
+	exports.createSecret = createSecret;
